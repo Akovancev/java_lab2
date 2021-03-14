1b24997b6ec7f7292b0fbfcfec40b122d1f2759f
Akovancev <apm120700@gmail.com>
2021-03-14 18:40:37 +0300

    Method of outputting the tree to the console

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -1,27 +1,27 @@
 public class Tree {
-    private char elem;
+    private String elem;
     private int priority;
     private Tree left;
     private Tree right;
 
     public Tree() {
-        this.elem = '&';
+        this.elem = "&";
         this.priority = 0;
         this.left = null;
         this.right = null;
     }
-    public Tree(char elem, int priority, Tree left, Tree right) {
+    public Tree(String elem, int priority, Tree left, Tree right) {
         this.elem = elem;
         this.priority = priority;
         this.left = left;
         this.right = right;
     }
 
-    public char getElem() {
+    public String getElem() {
         return elem;
     }
 
-    public void setElem(char elem) {
+    public void setElem(String elem) {
         this.elem = elem;
     }
 
@@ -47,5 +47,13 @@ public class Tree {
 
     public void setRight(Tree right) {
         this.right = right;
+    }
+
+    public static void printTree(Tree tree) {
+        System.out.println(tree.getElem() + " " + tree.getPriority());
+        if (tree.getLeft() != null)
+            printTree(tree.getLeft());
+        if (tree.getRight() != null)
+            printTree(tree.getRight());
     }
 }
